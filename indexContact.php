044bf99dae70a8169d5a9dbd24ceea093424846f
19ULmobDexep <s.zai@it-students.fr>
2019-11-27 15:36:33 +0000

    Nouveaux dossiers

---
+++ indexContact.php
@@ -0,0 +1,29 @@
+<?php
+ 
+
+include 'header.html';
+
+?>
+
+<h1>Contact</h1>
+
+<form action="indexContact.php" method="POST">
+
+<label for="Nom">Entrer votre Nom</label>
+<input type="Nom">
+
+<label for="Prénom">Entrer votre Prénom</label>
+<input type="Prénom">
+
+<label for="Adresse email">Entrer votre adresse email</label>
+<input type="Adresse email">
+
+<input type="submit" value="Envoyer">
+
+
+
+</form>
+
+<?php
+
+include 'Footer.html';
